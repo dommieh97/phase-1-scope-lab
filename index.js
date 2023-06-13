be99bd84311402420c74a06c84802a2a79ba2250
Domenique <domogenesis77@gmail.com>
2023-06-13 14:54:41 -0500

    wow

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+var customerName = `bob`
+
+const leastFavoriteCustomer = 'Jeff'
+function oob()
+{
+    return customerName
+};
+
+
+function upperCaseCustomerName()
+{
+    return customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer()
+{
+    bestCustomer = 'not bob'
+   
+    return bestCustomer
+};
+
+function overwriteBestCustomer()
+{
+    bestCustomer = 'maybe bob'
+
+    return bestCustomer
+};
+
+function  changeLeastFavoriteCustomer()
+{
+     leastFavoriteCustomer = 'bob'
+};
